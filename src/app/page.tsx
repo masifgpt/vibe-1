ebb535291425bb6eca548482a2487a0bd1b5c7c7
masifgpt <masifgpt@gmail.com>
2025-12-16 16:58:50 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Vibe-1</div>
+    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/asif2.JPG" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        M Asif
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Staff, M Asif
+      </div>
+    </figcaption>
+  </div>
+</figure></div>
   )
 }
 
